1323626c5461560da3dd5030ad17bf373dd359a9
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-06-01 10:43:34 +0700

    Menambahkan output untuk menandakan pembukaan dan pembacaan file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -42,3 +42,5 @@ int main()
     //menunjuk ke sebuah file dengan ekstensi .txt
     infile.open(NamaFile + ".txt", ios::in);
 
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
