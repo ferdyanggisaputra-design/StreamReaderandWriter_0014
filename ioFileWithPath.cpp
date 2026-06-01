2668db2d6541ea461134b572549c5f32dbe40652
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-06-01 10:40:39 +0700

    Menambahkan penutupan file setelah penulisan dan mempersiapkan untuk membuka file dalam mode membaca

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -33,3 +33,8 @@ int main()
         outfile << baris << endl;
         
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+    
+    //membuka file dalam mode membaca
+    ifstream infile;
