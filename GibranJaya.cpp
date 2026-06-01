cb82a4e3fbe489f9ac3deccf46eccab3d48d18a0
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-06-01 12:09:46 +0700

    Menambahkan konstruktor untuk mengisi data awal pada kelas TokoElektronik

--- GibranJaya.cpp
+++ GibranJaya.cpp
@@ -12,3 +12,11 @@ private:
     // Array berkapasitas 3 elemen untuk barang di etalase (Enkapsulasi)
     array<string, 3> etalase;
 
+public:
+    // Constructor untuk mengisi data awal secara otomatis
+    TokoElektronik() {
+        etalase[0] = "Laptop ASUS ROG";
+        etalase[1] = "Smartphone Samsung S24";
+        etalase[2] = "Smart TV LG 43 Inch";
+    }
+
