757faea9bba46ef700de8850d6c0edbbe73f9309
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-06-01 12:08:42 +0700

    Menambahkan struktur kelas privat TokoElektronik dengan array etalase

--- GibranJaya.cpp
+++ GibranJaya.cpp
@@ -5,3 +5,10 @@
 #include <stdexcept>
 #include <vector>
 using namespace std;
+
+// 1. STRUKTUR KELAS TOKOELEKTRONIK (OOP & ENKAPSULASI)
+class TokoElektronik {
+private:
+    // Array berkapasitas 3 elemen untuk barang di etalase (Enkapsulasi)
+    array<string, 3> etalase;
+
